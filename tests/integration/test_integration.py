3147b18595b87f61c1f4ab236663ca47d5b4d084
Jesse Looney <42242696+jesselooney@users.noreply.github.com>
2024-07-11 10:14:24 -0400

    Add beginnings of timing parsing

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -2,8 +2,10 @@ import json
 from pathlib import Path
 
 from vivado_report_parser.utilization import parse_utilization_report
+from vivado_report_parser.timing import parse_data_path_delay
 
 FIXTURES_DIR = Path(__file__).parent / 'fixtures'
+
 
 def test_utilization():
     with open(FIXTURES_DIR / 'utilization.rpt', 'r') as f:
@@ -17,3 +19,13 @@ def test_utilization():
 
     assert(report_json == expected_report_json)
 
+
+def test_data_path_delay():
+    with open(FIXTURES_DIR / 'timing.rpt', 'r') as f:
+        report_str = f.read()
+
+    delay = parse_data_path_delay(report_str)
+
+    assert(delay['total_ns'] == '3.005')
+    assert(delay['logic_ns'] == '1.059')
+    assert(delay['route_ns'] == '1.946')
--- vivado_report_parser/timing.py
+++ vivado_report_parser/timing.py
@@ -1,8 +1,27 @@
 import re
 
 
-def parse_timing_report(report_str: str):
-    # First parse at a high level, e.g. summary at top, table
-    # Then take a second pass parsing the string values of these fields
-    # into more structured data
-    pass    
+# def parse_timing_report(report_str: str):
+#     # First parse at a high level, e.g. summary at top, table
+#     # Then take a second pass parsing the string values of these fields
+#     # into more structured data
+#     match = re.search('Timing Report\n\n(?P<summary>[.\n]+)\n')
+#     summary = match.group('summary')
+
+#     lines = summary.split('\n')
+#     # Split at only the first ':' in the line
+#     entries = [line.split(':', 1) for line in lines]
+#     # Some lines in this report "wrap over" with a newline. We can find
+#     # them by searching for entries that have not been split into
+
+
+def parse_data_path_delay(report_str: str):
+    num = r'\d+\.?\d*'
+    match = re.search(rf'Data Path Delay:\s*({num})ns.*logic ({num})ns.*route ({num})ns', report_str)
+    return {
+        'total_ns': match.group(1),
+        'logic_ns': match.group(2),
+        'route_ns': match.group(3)
+    }
+    
+    
